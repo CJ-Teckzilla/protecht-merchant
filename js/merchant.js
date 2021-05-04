aa5b33694639754d8d58ee9bcb86d41a45b195f4
CJ-Teckzilla <78336581+CJ-Teckzilla@users.noreply.github.com>
2021-05-04 14:34:38 +0530

    Add files via upload

---
+++ js/merchant.js
@@ -0,0 +1,16 @@
+var baseURL = 'https://sandbox.moderntransact.com/protecht/';
+$(function(){
+    $('#merchant_form').bind('submit', function(e){
+         e.preventDefault();
+         // Validating Field
+         let data = $('#merchant_form').serializeArray();
+         const json_data = $.param(data);
+         var callbackurl = $('#btn_proceed').attr("callbackurl");
+         //Ajax call to retrieve token from server
+          $.get(baseURL+ "get_token.php", function(data) {
+            var url = baseURL+"hostedpage.html?"+json_data+"&token="+data+"&callbackurl="+callbackurl;
+            $('#payment_gateway').attr("src", url);
+            $('#staticBackdrop').modal('show');
+         });
+    });
+});
