ab685765b6d496d5cab5dd3786828f229144ac02
CJ-Teckzilla <78336581+CJ-Teckzilla@users.noreply.github.com>
2021-06-22 21:12:16 +0530

    Update merchant.js

--- js/merchant.js
+++ js/merchant.js
@@ -1,30 +1,49 @@
+// the below baseURL is of sandbox we need to change this url once the project is live
 var baseURL = 'https://sandbox.moderntransact.com/protecht/';
 $(function(){
+    // 1.Merchant needs to replace the below form id to their form id
     $('#merchant_form').bind('submit', function(e){
          e.preventDefault();
+         // sending all the input's which are present in the merchant form to map with the protecht parametes
          let mapped_data = map_params(this);
-         const query_string = $.param(mapped_data);
-         const callbackurl = "https://google.com";
+         const query_string = $.param(mapped_data);  // Converting object to query string
+         //2. Merchant will be redirected to the callbackurl after the successful response from protecht
+         const callbackurl = "https://google.com";  // Merchant has to replace the value of callbackurl
          const origin = window.location.origin;
+         // protecht public key is a required parameter to display protecht iframe
+         //3. Merchant has to replace the value of public_key to his public_key
          const public_key = "pk_sandbox_c24dc55e4d07719b80c0916ce8a28e4dbf6a048f"
          //Ajax call to retrieve token from server
+         //4. Merchant has to replace the baseURL + "get_token.php to their url
           $.get(baseURL+ "get_token.php", function(token) {
             var url = baseURL+"hostedpage.html?"+query_string+"&token="+token+"&callbackurl="+callbackurl+"&key="+public_key+"&origin="+origin;
-            $('#payment_gateway').attr("src", url);
-            $('#staticBackdrop').modal('show');
+            //5. Merchant has to replace the below iframe id to their iframe id
+            $('#payment_gateway').attr("src", url);  // Appending url to src attribute of an iframe
+            // We have use bootstrap modal popup form for example.
+            //6. Merchant has to replace the below code for displaying the modal popup form
+            $('#staticBackdrop').modal('show');  // opening modal popup form
          });
     });
 });
 
+// function to close iframe
+// How it works As the user clicks on the decline payment button a postmessage will be send to the merchant website
+// Then this below code will catch the message and hide the modal popup form
 window.addEventListener("message", (event) => {
+    // if the message received is Close iframe then it will close modal popup form
     if (event.data === "Close Iframe"){
+         //7. Merchant has to replace the below line of code to hide their modal popup form
          $('#staticBackdrop').modal('hide');
     }
 });
 
 
+// Function to map parameters
 function map_params(data){
     return {
+        //8. merchant has to replace the id here
+        // For eg: this.first_name, here first_name is id of the input field so merchant has to replace first_name
+        // to their respective field id
         "first_name": extract_value(this.first_name),
         "last_name": extract_value(this.last_name),
         "email": extract_value(this.email_form),
@@ -47,12 +66,12 @@ function map_params(data){
         "cost": extract_value(this.cost),
         "order_number": extract_value(this.order_number),
         "currency": extract_value(this.currency),
+        // value of below fields are hardcoded just for example
         "number": "4242 4242 4242 4242",
         "cvv": "589",
         "expiration_date"="12/24"
     };
 }
-
 
 function extract_value(field){
     try{
